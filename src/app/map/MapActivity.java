e7bfb97d98ed8ca5bf734baa6a20fe23336081a7
robsonfagundes <robsonfagundes@gmail.com>
2015-03-19 23:39:18 -0300

    first commit

---
+++ src/app/map/MapActivity.java
@@ -0,0 +1,13 @@
+package app.map;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class MapActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
